473f9eb921391d4cf614a781ce3f2416a908ab6a
Mariatta Wijaya <mariatta.wijaya@gmail.com>
2020-04-21 18:49:50 -0700

    Add code for how to say thanks when the app was installed.

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -49,7 +49,31 @@ async def webhook(request):
 @router.register("installation", action="created")
 async def repo_installation_added(event, gh, *args, **kwargs):
     installation_id = event.data["installation"]["id"]
-    pass
+    installation_access_token = await apps.get_installation_access_token(
+        gh,
+        installation_id=installation_id,
+        app_id=os.environ.get("GH_APP_ID"),
+        private_key=os.environ.get("GH_PRIVATE_KEY"),
+    )
+    sender_name = event.data["sender"]["login"]
+
+    for repo in event.data["repositories"]:
+
+        repo_full_name = repo["full_name"]
+        response = await gh.post(
+            f"/repos/{repo_full_name}/issues",
+            data={
+                "title": "Thanks for installing me",
+                "body": f"You're the best! @{sender_name}",
+            },
+            oauth_token=installation_access_token["token"],
+        )
+        issue_url = response["url"]
+        await gh.patch(
+            issue_url,
+            data={"state": "closed"},
+            oauth_token=installation_access_token["token"],
+        )
 
 
 if __name__ == "__main__":  # pragma: no cover
