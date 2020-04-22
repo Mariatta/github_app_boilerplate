fe1a15bc61078228ef1b67e62fbdef3201a3096d
Mariatta Wijaya <mariatta.wijaya@gmail.com>
2020-04-21 18:51:48 -0700

    Add code for how to say thanks when we receive a new PR

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -76,6 +76,41 @@ async def repo_installation_added(event, gh, *args, **kwargs):
         )
 
 
+@router.register("pull_request", action="opened")
+async def pr_opened(event, gh, *args, **kwargs):
+    issue_url = event.data["pull_request"]["issue_url"]
+    username = event.data["sender"]["login"]
+    installation_id = event.data["installation"]["id"]
+
+    installation_access_token = await apps.get_installation_access_token(
+        gh,
+        installation_id=installation_id,
+        app_id=os.environ.get("GH_APP_ID"),
+        private_key=os.environ.get("GH_PRIVATE_KEY"),
+    )
+    author_association = event.data["pull_request"]["author_association"]
+    if author_association == "NONE":
+        # first time contributor
+        msg = f"Thanks for your first contribution @{username}"
+    else:
+        # seasoned contributor
+        msg = f"Welcome back, @{username}. You are a {author_association}."
+    response = await gh.post(
+        f"{issue_url}/comments",
+        data={"body": msg},
+        oauth_token=installation_access_token["token"],
+    )
+
+
+if __name__ == "__main__":  # pragma: no cover
+    app = web.Application()
+
+    app.router.add_routes(routes)
+    port = os.environ.get("PORT")
+    if port is not None:
+        port = int(port)
+    web.run_app(app, port=port)
+
 if __name__ == "__main__":  # pragma: no cover
     app = web.Application()
 
