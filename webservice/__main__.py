fe9085aaf7255054d11c06b82242bdd6ea515a9e
Mariatta Wijaya <mariatta.wijaya@uplight.com>
2021-07-31 10:04:01 -0700

    Implement the installation webhook event handler.

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -20,7 +20,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", name="home")
 async def handle_get(request):
-    return web.Response(text="Hello world")
+    return web.Response(text="Hello PyLadies Tunis")
 
 
 @routes.post("/webhook")
@@ -49,7 +49,25 @@ async def webhook(request):
 @router.register("installation", action="created")
 async def repo_installation_added(event, gh, *args, **kwargs):
     installation_id = event.data["installation"]["id"]
-    pass
+
+    installation_access_token = await apps.get_installation_access_token(
+        gh,
+        installation_id=installation_id,
+        app_id=os.environ.get("GH_APP_ID"),
+        private_key=os.environ.get("GH_PRIVATE_KEY")
+    )
+    repo_name = event.data["repositories"][0]["full_name"]
+    url = f"/repos/{repo_name}/issues"
+    response = await gh.post(
+        url,
+                     data={
+        'title': 'Thanks for installing my bot',
+        'body': 'Thanks!',
+            },
+        oauth_token=installation_access_token["token"]
+                             )
+    print(response)
+
 
 
 if __name__ == "__main__":  # pragma: no cover
